d2f77b6064cef9896ef678e3d8341a3e14eec2ad
euguroglu <e.uguroglu@yahoo.com>
2021-05-14 17:12:46 +0300

    udf

--- Apache_Hive_Basics/UDF/udf_example.py
+++ Apache_Hive_Basics/UDF/udf_example.py
@@ -1,8 +1,12 @@
 import sys
 
-for line in sys.stdin:
-    line = line.strip()
-    first = line[0]
-    upper = first.upper()
-    new_line = upper+line[1:]
-    print(new_line)
+try:
+    for line in sys.stdin:
+        line = line.strip()
+        first = line[0]
+        upper = first.upper()
+        new_line = upper+line[1:]
+        print(new_line)
+
+except:
+    print(sys.exc_info())
