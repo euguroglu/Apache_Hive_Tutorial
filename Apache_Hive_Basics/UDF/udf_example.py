c4fafc5e626482a4c993739b33264186f0f863ab
euguroglu <e.uguroglu@yahoo.com>
2021-05-14 18:18:47 +0300

    hive-udf

--- Apache_Hive_Basics/UDF/udf_example.py
+++ Apache_Hive_Basics/UDF/udf_example.py
@@ -7,7 +7,7 @@ try:
         first = line[0]
         upper = first.upper()
         new_line = upper+line[1:]
-        print('\t'.join([new_line]))
+        print(','.join([new_line]))
 
 except:
     print(sys.exc_info())
